03ef29d540be42ca70aeae83e7fc58d658277484
Shreyas Sane <shreyasdbz@gmail.com>
2022-02-06 17:36:34 -0500

    Website metadata added

---
+++ pages/_document.tsx
@@ -0,0 +1,24 @@
+/** @format */
+
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="icon" href="favicon.ico" />
+          <link rel="manifest" href="/manifest.json" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+          <meta name="theme-color" content="#6366f1" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
